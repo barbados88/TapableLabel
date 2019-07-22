d2385a3423fdc551b2d206ac6aa4deb2500540c5
barbados88 <42581304+barbados88@users.noreply.github.com>
2019-07-22 15:33:53 +0300

    Update TapableLabel.swift
    - optimized

--- TapableLabel.swift
+++ TapableLabel.swift
@@ -2,52 +2,32 @@ import UIKit
 
 typealias TapableBlock = (String) -> Void
 
+@IBDesignable
 class TapableLabel: UILabel, UIGestureRecognizerDelegate {
-
-    private var pText: String = ""
-    private var aText: String = ""
-    private var lText: String = ""
-    var pFont: UIFont = UIFont.systemFont(ofSize: 14, weight: .medium)
-    var aFont: UIFont = UIFont.systemFont(ofSize: 14, weight: .bold)
-    var aColor: UIColor = .white
+    
+    private var pFont: UIFont = UIFont.systemFont(ofSize: 14, weight: .medium)
+    private var aFont: UIFont = UIFont.systemFont(ofSize: 14, weight: .bold)
 
     var tapBlock: TapableBlock? = nil
 
-    @IBInspectable var plainText: String = NSLocalizedString("I agree with", comment: "") {
-        didSet {
-            pText = plainText
-        }
-    }
-
-    @IBInspectable var attrText: String = NSLocalizedString("Terms and conditions Terms of Payment Processing, Privacy Policy", comment: "") {
-        didSet {
-            aText = attrText
-        }
-    }
-
-    @IBInspectable var partText: String = NSLocalizedString(": additional text here", comment: "") {
-        didSet {
-            lText = partText
-        }
-    }
-
-    @IBInspectable var attrColor: UIColor = .white {
-        didSet {
-            aColor = attrColor
-        }
-    }
-
-    func reloadInput() {
-        commonInit()
-    }
+    @IBInspectable var plainText: String = NSLocalizedString("policy_plain_text", comment: "")
+    @IBInspectable var attrText: String = NSLocalizedString("policy_attributedText", comment: "")
+    @IBInspectable var partText: String = ""
+    @IBInspectable var attrColor: UIColor = .white
 
     override func draw(_ rect: CGRect) {
         super.draw(rect)
         commonInit()
     }
-
-    override func layoutSubviews() {
-        super.layoutSubviews()
+    
+    required init(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)!
+        self.commonInit()
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.commonInit()
     }
 
     private func commonInit() {
@@ -57,10 +37,10 @@ class TapableLabel: UILabel, UIGestureRecognizerDelegate {
     }
 
     private func addAttributes() {
-        let attributedString = NSMutableAttributedString(string: "\(pText) \(aText) \(lText)")
-        attributedString.addAttributes([.font: pFont], range: NSMakeRange(0, pText.count))
-        attributedString.addAttributes([.font: aFont, .foregroundColor: aColor, .underlineStyle: 1], range: NSMakeRange(pText.count + 1, aText.count))
-        attributedString.addAttributes([.font: pFont], range: NSMakeRange(pText.count + 1 + aText.count + 1, lText.count))
+        let attributedString = NSMutableAttributedString(string: "\(plainText) \(attrText) \(partText)")
+        attributedString.addAttributes([.font: pFont], range: NSMakeRange(0, plainText.count))
+        attributedString.addAttributes([.font: aFont, .foregroundColor: attrColor, .underlineStyle: 1], range: NSMakeRange(plainText.count + 1, attrText.count))
+        attributedString.addAttributes([.font: pFont], range: NSMakeRange(plainText.count + 1 + attrText.count + 1, partText.count))
         attributedText = attributedString
     }
 
@@ -80,7 +60,9 @@ class TapableLabel: UILabel, UIGestureRecognizerDelegate {
         case .began: print("tap start")
         case .changed, .ended, .cancelled, .failed:
             indicateTap()
-            if tapBlock != nil { tapBlock?(pText) }
+            tapBlock?(plainText)
+            UIApplication.topViewController()?.performSegue(withIdentifier: "policy", sender: nil)
+        @unknown default: ()
         }
     }
 
@@ -93,9 +75,10 @@ class TapableLabel: UILabel, UIGestureRecognizerDelegate {
         switch gesture.state {
         case .possible, .changed: return
         case .began:
-            attributedString.addAttributes([.foregroundColor: aColor.withAlphaComponent(0.5)], range: NSMakeRange(pText.count + 1, aText.count))
+            attributedString.addAttributes([.foregroundColor: attrColor.withAlphaComponent(0.5)], range: NSMakeRange(plainText.count + 1, attrText.count))
         case .ended, .cancelled, .failed:
-            attributedString.addAttributes([.foregroundColor: aColor.withAlphaComponent(1.0)], range: NSMakeRange(pText.count + 1, aText.count))
+            attributedString.addAttributes([.foregroundColor: attrColor.withAlphaComponent(1.0)], range: NSMakeRange(plainText.count + 1, attrText.count))
+        @unknown default: ()
         }
         attributedText = attributedString
         setNeedsDisplay()
@@ -107,7 +90,7 @@ class TapableLabel: UILabel, UIGestureRecognizerDelegate {
                 return
         }
         let attributedString = NSMutableAttributedString(attributedString: text)
-        attributedString.addAttributes([.foregroundColor: aColor.withAlphaComponent(0.5)], range: NSMakeRange(pText.count + 1, aText.count))
+        attributedString.addAttributes([.foregroundColor: attrColor.withAlphaComponent(0.5)], range: NSMakeRange(plainText.count + 1, attrText.count))
         attributedText = attributedString
         DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
             self.indicateState(attributedString)
@@ -116,7 +99,7 @@ class TapableLabel: UILabel, UIGestureRecognizerDelegate {
     }
 
     private func indicateState(_ attributedString: NSMutableAttributedString) {
-        attributedString.addAttributes([.foregroundColor: aColor.withAlphaComponent(1.0)], range: NSMakeRange(pText.count + 1, aText.count))
+        attributedString.addAttributes([.foregroundColor: attrColor.withAlphaComponent(1.0)], range: NSMakeRange(plainText.count + 1, attrText.count))
         attributedText = attributedString
     }
 
